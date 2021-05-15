5d41a8ba4f03f26c285f38916675d11c62934a29
hitesh_kothari <hitesh.kothari@paptronics.com>
2021-05-15 14:05:34 +0530

    issue resolved

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { AppService } from './app.service';
 import { NgbModal, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
-  selector: 'app-ubold',
+  selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
